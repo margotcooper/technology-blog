5aa7c00eae2a3640762b39b5177cbc0f44a6403a
Margot Cooper Gruen <margotgruen@gmail.com>
2022-09-22 16:39:07 -0700

    initial commit with file structure in line with MVC

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+module.exports = sequelize;
